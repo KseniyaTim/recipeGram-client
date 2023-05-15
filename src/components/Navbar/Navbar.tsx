9f203173bb9f6f0f611e32a87c5696d28f32d916
Kseniya Timoshchuk <111976842+KseniyaTim@users.noreply.github.com>
2023-05-16 00:08:16 +0300

    fixing navbar styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,30 +2,64 @@ import { FC, useEffect } from "react";
 
 //routing
 import { useHistory } from "react-router-dom";
-import { ADD_RECIPE_ROUTE, HOME_ROUTE, PROFILE_ROUTE } from "../../consts/routeConsts";
+import {
+  ADD_RECIPE_ROUTE,
+  HOME_ROUTE,
+  PROFILE_ROUTE,
+} from "../../consts/routeConsts";
 
 //css
 import "./Navbar.css";
 
 const Navbar: FC = () => {
-  const codedId = localStorage.getItem("userIdToken");
+
+  const authToken = localStorage.getItem("userIdToken");
 
   useEffect(() => {
+    
+  }, []);
 
-  }, [codedId])
-  
-  
-
-
-  const history = useHistory()
+  const history = useHistory();
   return (
     <>
       <div className="navbarWrapper">
-        <div className="mainTitle" onClick={() => {history.push(HOME_ROUTE)}}>RecipeGram</div>
-        <div>
-          <> {codedId ? <span onClick={() => {localStorage.clear()}}>log out</span> : ''}</>
-          <span className="material-symbols-outlined icon" onClick={() => {history.push(ADD_RECIPE_ROUTE)}}>add</span>
-          <span className="material-symbols-outlined icon"  onClick={() => {history.push(PROFILE_ROUTE)}}>account_circle</span>
+        <div
+          className="mainTitle"
+          onClick={() => {
+            history.push(HOME_ROUTE);
+          }}
+        >
+          RecipeGram
+        </div>
+        <div className="rightSide">
+          {authToken ? (
+            <span
+              onClick={() => {
+                localStorage.clear();
+              }}
+              className="logout"
+            >
+              log out
+            </span>
+          ) : (
+            ""
+          )}
+          <span
+            className="material-symbols-outlined icon"
+            onClick={() => {
+              history.push(ADD_RECIPE_ROUTE);
+            }}
+          >
+            add
+          </span>
+          <span
+            className="material-symbols-outlined icon"
+            onClick={() => {
+              history.push(PROFILE_ROUTE);
+            }}
+          >
+            account_circle
+          </span>
         </div>
       </div>
     </>
